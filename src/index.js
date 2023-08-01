f1066a1b00b6732ccebd06e387f217e66b27dea0
saboheb <100vbo21@gmail.com>
2023-08-01 15:30:47 +0400

    feat: array solution

--- src/index.js
+++ src/index.js
@@ -1,55 +1,39 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
 
-let str = toString(number)
+  let unitTeens = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-  if ((str.length == 1) || (number == 0)) {
-  return 'zero';
-  } else if ((str.length == 1) || (number == 1)) {
-  return 'one';
-  } else if ((str.length == 1) || (number == 2)) {
-  return 'two';
-  } else if ((str.length == 1) || (number == 3)) {
-  return 'three';
-  } else if ((str.length == 1) || (number == 4)) {
-  return 'four';
-  } else if ((str.length == 1) || (number == 5)) {
-  return 'five';
-  } else if ((str.length == 1) || (number == 6)) {
-  return 'six';
-  } else if ((str.length == 1) || (number == 7)) {
-  return 'seven';
-  } else if ((str.length == 1) || (number == 8)) {
-  return 'eight';
-  } else if ((str.length == 1) || (number == 9)) {
-  return 'nine';
-  } else if ((str.length == 2) || (number == 10)) {
-  return 'ten';
-  } else if ((str.length == 2) || (number == 11)) {
-  return 'eleven';
-  } else if ((str.length == 2) || (number == 12)) {
-  return 'twelve';
-  } else if ((str.length == 2) || (number == 13)) {
-  return 'thirteen';
-  } else if ((str.length == 2) || (number == 14)) {
-  return 'fourteen';
-  } else if ((str.length == 2) || (number == 15)) {
-  return 'fiveteen';
-  } else if ((str.length == 2) || (number == 16)) {
-  return 'sixteen';
-  } else if ((str.length == 2) || (number == 17)) {
-    return 'seventeen';
-  } else if ((str.length == 2) || (number == 18)) {
-  return 'eightteen';
-  } else if ((str.length == 2) || (number == 19)) {
-  return 'nineteen';
-  } else if ((str.length == 2) || (number == 20)) {
-  return 'twenty';
-  } else if ((str.length == 2) || (number == 21)) {
-  return 'twenty one';
-  
-  
+ let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
+ let hundred = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
 
-  } else if ((str.length == 3) || (number == 999)) {
-  return 'nine hundred ninety nine';
-  } }
+if (number === 0) {
+    return 'zero';
+}
+
+if (number < 20) {
+    return unitTeens[number - 1];
+}
+
+if (number > 19 && number < 100 && number % 10 === 0) {
+    return tens[number / 10 - 2];
+}
+
+if (number > 19 && number < 100 && number % 10 !== 0) {
+    return tens[(Math.floor(number / 10)) - 2] + ' ' + unitTeens[number - (Math.floor(number / 10)) * 10 - 1];
+}
+
+if (number > 99 && number % 100 === 0) {
+    return hundred[number / 100 - 1];
+}
+
+if (number > 99 && (number - (Math.floor(number / 100)) * 100) < 20) {
+    return hundred[Math.floor(number / 100) - 1] + ' ' + unitTeens[number - (Math.floor(number / 100) * 100) - 1];
+}
+
+if (number > 100 && number % 10 === 0) {
+    return hundred[Math.floor(number / 100) - 1] + ' ' + tens[(number - (Math.floor(number / 100) * 100)) / 10 - 2];
+}
+
+return hundred[Math.floor(number / 100) - 1] + ' ' + tens[Math.floor((number - (Math.floor(number / 100)) * 100) / 10)- 2] + ' ' + unitTeens[number - (Math.floor(number / 10)) * 10 - 1];
+
+}
